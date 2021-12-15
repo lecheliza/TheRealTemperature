c32e52a788fa6ec7a82b7b00a285e917d295c601
lecheliza <lechoosz@gmail.com>
2021-12-15 15:34:16 +0100

    modified the script

--- wifiService/main.py
+++ wifiService/main.py
@@ -8,6 +8,7 @@ read_api_key = "REG5C56EQ2XIF2NL"
 channel_id = '1601670'
 
 while True:
+    time.sleep(15)
     TS = urllib.request.urlopen(
         "https://api.thingspeak.com/channels/1601670/feeds.json?api_key=REG5C56EQ2XIF2NL&results=2")
     response = TS.read()
@@ -15,6 +16,12 @@ while True:
     print(data['feeds'])
     for value in data['feeds']:
         arduino.write(bytes(value['field1'], 'utf-8'))
-        time.sleep(15)
-
     TS.close()
+    TS = urllib.request.urlopen(
+        "https://api.thingspeak.com/channels/1608807/feeds.json?api_key=GA4Y4O7RWEY31YFX&results=2")
+    response = TS.read()
+    data = json.loads(response)
+    print(data['feeds'])
+    for value in data['feeds']:
+        arduino.write(bytes(value['field1'], 'utf-8'))
+
